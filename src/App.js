4115ff67cea9fd8506e0f6eea5eacd6aa6da733c
Nikhil Arora <narora200@gmail.com>
2018-08-18 07:46:59 +0530

    [Feat]: Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Projects Tracker
+      </div>
+    );
+  }
+}
+
+export default App;
