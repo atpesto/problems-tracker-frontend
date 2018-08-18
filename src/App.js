1433125e68d4b0e787502e1ebd1d5b9670ab96b7
Nikhil Arora <narora200@gmail.com>
2018-08-18 08:07:52 +0530

    [Feat]: Integrated Ant-Design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Layout } from 'antd';
 
+
+const { Header, Footer, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div>
-        Projects Tracker
+        <Layout
+          style={{
+            minHeight: '100vh',
+          }}
+        >
+          <Header
+            style={{
+              color: '#fff',
+              textAlign: 'center',
+              fontSize: '2em',
+            }}
+          >
+            Competitive Programming
+          </Header>
+          <Content>
+            Projects Tracker
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Made with <span role="img" aria-label="heart">❤️</span>
+          </Footer>
+        </Layout>
       </div>
     );
   }
